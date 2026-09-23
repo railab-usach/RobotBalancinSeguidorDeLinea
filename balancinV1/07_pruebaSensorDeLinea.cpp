76af67e10ca370631a89434ea62a741df16bef88
railab-usach <railab.usach@gmail.com>
2026-09-22 22:09:36 -0300

    Create 07_pruebaSensorDeLinea.cpp

---
+++ balancinV1/07_pruebaSensorDeLinea.cpp
@@ -0,0 +1,31 @@
+/*  
+* El sensor de línea es analógico. Tras unas pruebas, la
+  zona blanca marca alrededor de 2500 y en la zona negra 4000.
+*/
+
+int D1 = 36;
+int D2 = 39;
+int D3 = 34;
+int D4 = 35;
+int D5 = 32;
+int D6 = 33;
+int D7 = 25;
+int D8 = 36;
+
+// ----- CONFIG INICIAL -----//
+void setup() {
+  Serial.begin(115200);
+}
+
+
+void loop() {
+  Serial.print(analogRead(D1));
+  Serial.print(analogRead(D2));
+  Serial.print(analogRead(D3));
+  Serial.print(analogRead(D4));
+  Serial.print(analogRead(D5));
+  Serial.print(analogRead(D6));
+  Serial.print(analogRead(D7));
+  Serial.println(analogRead(D8));
+  delay(200);
+}
